df7c2f1e1e70588b73b64b63d0edcccc278952e0
Max Sobroza <msobroza>
2019-06-10 20:24:58 +0200

    Speaker reco

--- audioset_sagemaker/audioset_class/runme.sh
+++ audioset_sagemaker/audioset_class/runme.sh
@@ -8,13 +8,16 @@ WORKSPACE="./pub_keras_audioset_classification"
 
 BACKEND="keras"     # 'pytorch' | 'keras'
 
-MODEL_TYPE="adaptative_pooling"    # 'decision_level_max_pooling'
+MODEL_TYPE="decision_level_multi_attention_embedding"    # 'decision_level_max_pooling'
                                                 # | 'decision_level_average_pooling'
                                                 # | 'decision_level_single_attention'
                                                 # | 'decision_level_multi_attention'
+						# | 'adaptative_pooling'
+						# | 'decision_level_multi_attention_embedding' 
+EMBEDDINGS_FILEPATH="~/oso/audioset_label_clustering/onto_df.pkl"
 
 # Train
-CUDA_VISIBLE_DEVICES=0 python $BACKEND/main.py --data_dir=$DATA_DIR --workspace=$WORKSPACE --model_type=$MODEL_TYPE train
+CUDA_VISIBLE_DEVICES=0 python $BACKEND/main.py --data_dir=$DATA_DIR --embeddings=$EMBEDDINGS_FILEPATH --workspace=$WORKSPACE --model_type=$MODEL_TYPE train
 
 # Calculate averaged statistics. 
-python $BACKEND/main.py --data_dir=$DATA_DIR --workspace=$WORKSPACE --model_type=$MODEL_TYPE get_avg_stats
+#python $BACKEND/main.py --data_dir=$DATA_DIR --workspace=$WORKSPACE --model_type=$MODEL_TYPE get_avg_stats
